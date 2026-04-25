ef255f541841d0e98cbc8cc36e7eea29464d7890
David Gao <david.04.gao@gmail.com>
2026-04-25 19:01:15 -0400

    starting level design and creation

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public void GoNorth()
+    {
+        
+    }
+
+    public void East()
+    {
+        
+    }
+
+    public void South()
+    {
+        
+    }
+
+    public void West()
+    {
+        
+    }
+}
